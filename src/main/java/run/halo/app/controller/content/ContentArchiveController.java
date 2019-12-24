4e137033046cdf9467228ca0bf9a867116cd9c78
luiz <donglu@cs.sh.cn>
2019-12-24 17:45:34 +0800

    提供rest 查询接口

--- src/main/java/run/halo/app/controller/content/ContentArchiveController.java
+++ src/main/java/run/halo/app/controller/content/ContentArchiveController.java
@@ -2,7 +2,10 @@ package run.halo.app.controller.content;
 
 import cn.hutool.core.util.IdUtil;
 import cn.hutool.core.util.PageUtil;
+import com.google.gson.JsonObject;
+import com.qiniu.util.Json;
 import lombok.extern.slf4j.Slf4j;
+import org.json.JSONObject;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -15,6 +18,7 @@ import run.halo.app.cache.StringCacheStore;
 import run.halo.app.cache.lock.CacheLock;
 import run.halo.app.exception.ForbiddenException;
 import run.halo.app.model.entity.Category;
+import run.halo.app.model.entity.Option;
 import run.halo.app.model.entity.Post;
 import run.halo.app.model.entity.Tag;
 import run.halo.app.model.enums.PostStatus;
@@ -24,6 +28,7 @@ import run.halo.app.service.*;
 import run.halo.app.utils.MarkdownUtils;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -104,6 +109,27 @@ public class ContentArchiveController {
         return themeService.render("archives");
     }
 
+    @ResponseBody
+    @GetMapping(value = "findPage/{page}",produces = "application/json; charset=utf-8")
+    public String findAarchivesPage(@PathVariable(value = "page") Integer page,
+                           @SortDefault(sort = "createTime", direction = DESC) Sort sort) {
+        Pageable pageable = PageRequest.of(page - 1, optionService.getPostPageSize(), sort);
+
+        Page<Post> postPage = postService.pageBy(PostStatus.PUBLISHED, pageable);
+        Page<PostListVO> postListVos = postService.convertToListVo(postPage);
+        int[] pageRainbow = PageUtil.rainbow(page, postListVos.getTotalPages(), 3);
+
+      /*  model.addAttribute("is_archives", true);
+        model.addAttribute("pageRainbow", pageRainbow);
+        model.addAttribute("posts", postListVos);*/
+      List<Post> postList=new ArrayList<>();
+      for(Post post:postPage){
+          postList.add(post);
+      }
+     return JSONObject.valueToString(postList);
+    }
+
+
     /**
      * Render post page.
      *
@@ -172,6 +198,78 @@ public class ContentArchiveController {
         }
 
         return themeService.render("post");
+    }
+
+
+    /**
+     * Render post page.
+     *
+     * @param url     post slug url.
+     * @param preview preview
+     * @param token   preview token
+     * @return template path: themes/{theme}/post.ftl
+     */
+    @ResponseBody
+    @GetMapping(value = "findByUrlId/{url}",produces = "application/json; charset=utf-8")
+    public String findByUrlId(@PathVariable("url") String url,
+                       @RequestParam(value = "preview", required = false, defaultValue = "false") boolean preview,
+                       @RequestParam(value = "intimate", required = false, defaultValue = "false") boolean intimate,
+                       @RequestParam(value = "token", required = false) String token,
+                       @RequestParam(value = "cp", defaultValue = "1") Integer cp,
+                       @SortDefault(sort = "createTime", direction = DESC) Sort sort) {
+        Post post;
+        if (preview) {
+            post = postService.getBy(PostStatus.DRAFT, url);
+        } else if (intimate) {
+            post = postService.getBy(PostStatus.INTIMATE, url);
+        } else {
+            post = postService.getBy(PostStatus.PUBLISHED, url);
+        }
+
+        // if this is a preview url.
+        if (preview) {
+            // render markdown to html when preview post
+            post.setFormatContent(MarkdownUtils.renderHtml(post.getOriginalContent()));
+
+            // verify token
+            String cachedToken = cacheStore.getAny("preview-post-token-" + post.getId(), String.class).orElseThrow(() -> new ForbiddenException("该文章的预览链接不存在或已过期"));
+
+            if (!cachedToken.equals(token)) {
+                throw new ForbiddenException("该文章的预览链接不存在或已过期");
+            }
+        }
+
+        // if this is a intimate url.
+        if (intimate) {
+            // verify token
+            String cachedToken = cacheStore.getAny(token, String.class).orElseThrow(() -> new ForbiddenException("您没有该文章的访问权限"));
+            if (!cachedToken.equals(token)) {
+                throw new ForbiddenException("您没有该文章的访问权限");
+            }
+        }
+/*
+        postService.getNextPost(post.getCreateTime()).ifPresent(nextPost -> model.addAttribute("nextPost", nextPost));
+        postService.getPrePost(post.getCreateTime()).ifPresent(prePost -> model.addAttribute("prePost", prePost));*/
+
+        List<Category> categories = postCategoryService.listCategoriesBy(post.getId());
+        List<Tag> tags = postTagService.listTagsBy(post.getId());
+
+        Page<BaseCommentVO> comments = postCommentService.pageVosBy(post.getId(), PageRequest.of(cp, optionService.getCommentPageSize(), sort));
+        JSONObject jsonObject=new JSONObject();
+
+
+        jsonObject.put("is_post", true);
+        jsonObject.put("post", postService.convertToDetailVo(post));
+        jsonObject.put("categories", categories);
+        jsonObject.put("tags", tags);
+        jsonObject.put("comments", comments);
+
+        if (preview) {
+            // refresh timeUnit
+            cacheStore.putAny("preview-post-token-" + post.getId(), token, 10, TimeUnit.MINUTES);
+        }
+
+        return jsonObject.toString();
     }
 
     @GetMapping(value = "{url}/password")
